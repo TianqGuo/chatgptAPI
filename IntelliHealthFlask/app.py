d8cfe62c8c4213fac8696ec70798f511533b2b03
Tianquan Guo <tianquanguo@gmail.com>
2023-06-29 21:41:24 -0700

    Added get gpt service, langchain service and get model service

--- IntelliHealthFlask/app.py
+++ IntelliHealthFlask/app.py
@@ -5,8 +5,12 @@ app = Flask(__name__)
 
 @app.route('/api/v1/health')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Status: Healthy, Version: 1.0'
 
+
+@app.route('/api/v1/chat/<chat_content>')
+def chat(chat_content):  # put application's code here
+    return chat_content
 
 @app.route('/api/v1/chat/<chat_content>')
 def chat(chat_content):  # put application's code here
--- IntelliHealthFlask/utils/utils.py
+++ IntelliHealthFlask/utils/utils.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 import openai
+import pandas as pd
+import numpy as np
+import tensorflow as tf
 
 
 class Client:
@@ -17,15 +20,46 @@ class Client:
         # print(self.key)
         openai.api_key = self.key
 
-    def ask_question(self, prompt):
+    def ask_question(self, prompt, max_tokens=60, n=1, stop=None, temperature=0.5):
         # Generate text
         response = openai.Completion.create(
             engine=self.engine,
             prompt=prompt,
-            max_tokens=60,
-            n=1,
-            stop=None,
-            temperature=0.5,
+            max_tokens=max_tokens,
+            n=n,
+            stop=stop,
+            temperature=temperature,
         )
 
         return response
+
+
+class ModelPrediction:
+    def __init__(self, model_path=os.getenv("model_path")):
+        self.model = tf.keras.models.load_model(model_path)
+        self.config = self.model.get_config()
+
+    def predict(self, x_input):
+        test_output = self.model.predict(x_input)
+        return test_output
+
+    def get_config(self):
+        return self.config
+
+    def get_model(self):
+        return self.model
+
+    def sanity_test(self):
+        df = pd.read_csv(os.getenv("preprocessed_data_path"))
+        y = ['metformin', 'repaglinide', 'nateglinide', 'chlorpropamide', 'glimepiride', 'acetohexamide',
+                  'glipizide', 'glyburide', 'tolbutamide', 'pioglitazone', 'rosiglitazone', 'acarbose', 'miglitol',
+                  'troglitazone', 'tolazamide',
+                  'examide', 'citoglipton', 'insulin', 'glyburide-metformin', 'glipizide-metformin',
+                  'glimepiride-pioglitazone', 'metformin-rosiglitazone', 'metformin-pioglitazone']
+        x = df.drop(y, axis=1)
+        test_input = np.array(x.iloc[0])
+        test_input = test_input[None, :]  # shape 1x4
+        print("Test input shape is: ", test_input.shape)
+        test_output = self.model.predict(test_input)
+        print("Test output shape is: ", test_output.shape)
+        print("Sanity test passed!")
