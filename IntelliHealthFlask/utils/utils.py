fe91ab8bc17d888f4f334ef3753508f2badb645d
Tianquan Guo <tianquanguo@gmail.com>
2023-07-29 09:04:22 -0700

    Modified the pinecone_service and VectorDB classes

--- IntelliHealthFlask/utils/utils.py
+++ IntelliHealthFlask/utils/utils.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow as tf
 import joblib
 import pinecone
+from openai.embeddings_utils import get_embedding
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -75,26 +76,105 @@ class ModelPrediction:
 
 
 class VectorDB:
-    def __init__(self, vector_path="vector_path"):
-        load_dotenv()
-        pinecone.init(api_key=os.getenv("PINECONE_KEY"), environment=os.getenv("PINECONE_ENVIRON"))
-        self.cur_index = pinecone.Index('vector-database')
+    def __init__(self, index_name="vectordatabase"):
+        set_openai_key()
+        self.pinecone_key = os.getenv("PINECONE_KEY")
+        self.pinecone_env = os.getenv("PINECONE_ENVIRON")
+        pinecone.init(api_key=self.pinecone_key, environment=self.pinecone_env)
+        self.index = pinecone.Index(index_name)
+
+    def get_embeddings(self, input_text, is_upsert=False):
+        input_text = 'user: ' + input_text
+        # model = "text-embedding"
+        # if not is_upsert:
+        model = "text-embedding-ada-002"
+        # print("current embedding model is: ", model)
+        embedding = openai.Embedding.create(
+            input=input_text,
+            model=model
+        )
+        # print the embedding (length = 1536)
+        vector = embedding["data"][0]["embedding"]
+        # print(vector)
+        # print(len(vector))
+        return vector
+
+    def query_match(self, input_text):
+        vector = self.get_embeddings(input_text)
+
+        search_response = self.index.query(top_k=5, vector=vector, include_metadata=True)
+
+        # print(search_response['matches'])
+
+        # context, url = self.get_highest_score(search_response['matches'])
+        context = self.get_highest_score(search_response['matches'])
+
+        # print(context)
+
+        prompt = f"Answer the question based on the context below:\n\n{context}\n\nQuestion: {input_text}\nAnswer:"
+
+        return prompt
+
+    def get_highest_score(self, items):
+        highest_score_item = max(items, key=lambda item: item["score"])
+
+        print(highest_score_item)
+
+        if highest_score_item["score"] > 0.8:
+            # return highest_score_item["metadata"]['text'], highest_score_item["metadata"]['url']
+            return highest_score_item
+        else:
+            return ""
+
+    def list_indexes(self):
+        print(pinecone.list_indexes())
+
+    def create_index(self, index_name, dimension):
+        pinecone.create_index(name=index_name, dimension=dimension)
+
+    def insert_data(self, index_name, data):
+        self.index.upsert(items=data)
 
     def sanity_test(self):
-        index = pinecone.Index('vector-database')
-        index.upsert([
-            ("A", [i for i in range(1024)]),
-            ("B", [i for i in range(1024, 0, -1)]),
-            ("C", (np.ones(1024) * 3).tolist()),
-            ("D", [i for i in range(0, 2048, 2)]),
-            ("E", [i for i in range(2048, 0, -2)]),
+        # test 1
+        self.index.upsert([
+            ("A", [i for i in range(1536)]),
+            ("B", [i for i in range(1536, 0, -1)]),
+            ("C", (np.ones(1536) * 3).tolist()),
+            ("D", [i for i in range(0, 3072, 2)]),
+            ("E", [i for i in range(3072, 0, -2)]),
         ])
-        response = index.query(
-            vector=(np.ones(1024) * 0.3).tolist(),
+        response1 = self.index.query(
+            vector=(np.ones(1536) * 0.3).tolist(),
             top_k=3,
             include_values=False
         )
-        print(response)
+        print(response1)
+
+        # test 2
+
+        self.index.upsert([("sanity test2", self.get_embeddings("What is diabetes?"))])
+
+        self.index.upsert([("sanity test3", self.get_embeddings("How can I help you?"))])
+
+        self.index.upsert([("sanity test4", self.get_embeddings("What is wrong with you?"))])
+
+        response2 = self.index.query(
+            vector=self.get_embeddings("What is diabetes?"),
+            top_k=3,
+            include_values=False
+        )
+
+        print(response2)
+
+        response3 = self.index.query(
+            vector=self.get_embeddings("is diabetes what?"),
+            top_k=3,
+            include_values=False
+        )
+
+        print(response3)
+
         print("Sanity test passed!")
 
 
@@ -107,4 +187,5 @@ if __name__ == "__main__":
     # print(np.random.rand(1, 74))
     cur_vector_db = VectorDB()
     cur_vector_db.sanity_test()
-    print(cur_vector_db.cur_index.describe_index_stats())
+    print(cur_vector_db.index.describe_index_stats())
+    cur_vector_db.query_match("what is diabetes?")
